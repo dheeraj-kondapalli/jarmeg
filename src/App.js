c5844c31067695f36ad558dcfadf86b301e99990
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-01-31 00:20:31 -0600

    chng

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import LoginandSignUp from './Pages/LoginSignup/LoginandSignUp';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './Layout/Layout';
+import LoginPage from './Pages/LoginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/home' element = {<LoginandSignUp/>}/>
+          <Route path='/login' element = {<LoginPage/>}/>
+        </Routes>
+      </Layout>
+      </BrowserRouter>
+      
     </div>
   );
 }
